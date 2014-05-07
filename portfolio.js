01005e0248e4a597f4c88ade4d6426956237d9d2
Jeremy Fine <jjfine@gmail.com>
2014-05-07 07:35:47 +0700

    Move AddAssetForm to its own file.

--- portfolio.js
+++ portfolio.js
@@ -1,40 +1,4 @@
 /** @jsx React.DOM **/
-var AddAssetForm = React.createClass({
-  getInitialState: function() {
-    return { symbol: '', shares: ''};
-  },
-
-  onChange: function() {
-    this.setState(
-        { symbol: this.refs.symbol.getDOMNode().value
-        , shares: this.refs.shares.getDOMNode().value
-    });
-  },
-
-  handleSubmit: function(e) {
-    e.preventDefault();
-    this.props.addAsset(this.state.symbol,this.state.shares);
-  },
-  
-  render: function() {
-    return (
-      <form onSubmit={this.handleSubmit} className="form">
-        <div className="form-group row">
-          <div className="col-xs-3">
-            <input type="text" onChange={this.onChange} placeholder="Sybmol" ref="symbol" className="form-control" />
-          </div>
-          <div className="col-xs-3">
-            <input type="text" onChange={this.onChange} placeholder="Shares" ref="shares" className="form-control" />
-          </div>
-          <div className="col-xs-2">
-            <button className="btn btn-default">Add</button>
-          </div>
-        </div>
-      </form>
-    );
-  }
-});
-
 var SharesInput = React.createClass({
   onChange: function() {
     newShares = this.refs.shares.getDOMNode().value;
