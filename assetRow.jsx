d998dd2b00bfed1bb1037154e86697987fc3c418
Jeremy Fine <jjfine@gmail.com>
2014-05-07 07:40:42 +0700

    Move assetRow to its own file.

---
+++ assetRow.jsx
@@ -0,0 +1,43 @@
+/** @jsx React.DOM **/
+var AssetRow = React.createClass({
+  changeShares:function(shares) {
+    this.props.changeSharesByIndex(this.props.key, shares);
+  },
+
+  changeClass:function(assetClass) {
+    this.props.changeClassByIndex(this.props.key, assetClass);
+  },
+
+  deleteRow: function() {
+    this.props.deleteAsset(this.props.key);
+  },
+
+  priceOrLoading: function() {
+    if (this.props.price === -1) {
+      return "Loading..."
+    }
+    return formatDollars(this.props.price)
+  },
+
+  valueOrLoading: function() {
+    if (this.props.price === -1) {
+      return "Loading..."
+    }
+    return formatDollars(this.props.price*this.props.asset.shares)
+  },
+
+  render: function() {
+    return (
+      <tr>
+        <td>{this.props.asset.symbol}</td>
+        <td><SharesInput changeShares={this.changeShares} shares={this.props.asset.shares} /></td>
+        <td>{this.priceOrLoading()}</td>
+        <td>{this.valueOrLoading()}</td>
+        <td><AssetClassSelect changeClass={this.changeClass} assetClass={this.props.asset.assetClass} /></td>
+        <td><a href="javascript:void(0);" onClick={this.deleteRow}>Delete</a></td>
+      </tr>
+    );
+  }
+});
+
+
